54aba2981142f75a3d80510c5b437bcb83d56658
Marcos Churi <51281680+markchure@users.noreply.github.com>
2021-04-26 01:34:19 -0300

    Update ejercicio4.js

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,30 +1,11 @@
 /*Implementar una función que dado 3 parámetros numéricos, retorne verdadero si el
-último dígito de cada uno de los números ingresados son iguales.*/
-
+último dígito de cada uno de los números ingresados son iguales*/
 function lastDigitEqual(param1, param2, param3) {
   let ultimoDigito1 = param1.charAt(param1.length - 1);
   let ultimoDigito2 = param2.charAt(param2.length - 1);
   let ultimoDigito3 = param3.charAt(param3.length - 1);
 
-  let isValid = false;
-
-  if (ultimoDigito1 == ultimoDigito2) {
-    isValid = true;
-
-    if (isValid && ultimoDigito1 == ultimoDigito3) {
-      isValid = true;
-
-      if (isValid && ultimoDigito2 == ultimoDigito3) {
-        isValid = true;
-      } else {
-        isValid = false;
-      }
-    } else {
-      isValid = false;
-    }
-  }
-
-  return isValid;
+  return ultimoDigito1 == ultimoDigito2 && ultimoDigito1 == ultimoDigito3 && ultimoDigito2 == ultimoDigito3
 }
 
 let param1 = prompt("Ingrese parametro1: ");
